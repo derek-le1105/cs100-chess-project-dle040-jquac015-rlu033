3fc50633e5321142e050231170f5f93572b711a4
Lyran312 <ryanlu312@gmail.com>
2020-12-06 12:55:47 -0800

    Testing BoardFactory

---
+++ testing/RLu_test.hpp
@@ -0,0 +1,19 @@
+#ifndef __RLU_TEST__
+#define __RLU_TEST__
+
+#include "gtest/gtest.h"
+#include "../Board.hpp"
+#include "../BoardArray.h"
+#include "../Piece.h"
+
+TEST(FactoryTest, NormalTest) {
+    Piece* array[8][8];
+    BoardFactory b;
+    b.CreateBoard(array, NORMAL);
+
+    ASSERT_NE(array, nullptr);
+    EXPECT_EQ(array[0][0]->getType(), PType::rtype)
+    EXPECT_EQ(array[0][0]->getAlignment(), BLACK)
+}
+
+#endif
