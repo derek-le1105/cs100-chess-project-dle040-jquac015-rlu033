bd59170eefa42bd0c97804fd67117bc2b5bf159c
Lyran312 <ryanlu312@gmail.com>
2020-12-06 17:15:13 -0800

    Added FreeBoard() to Factory

--- Board.hpp
+++ Board.hpp
@@ -15,10 +15,8 @@ class BoardFactory {
             }
         }
     }
-    //changes array in argument, does not create a new one
-    //array passed in MUST be all nullptr
-    //Fischer-random needs test cases, and this function needs retesting
-    void CreateBoard(Piece* array[][8], ChessType type = NORMAL) {
+    //should be used to free all pointers on the board
+    void FreeBoard(Piece* array[][8]) {
         int i, j;
         for (i = 0; i < 8; i++) {
             for (j = 0; j < 8; j++) {
@@ -27,6 +25,12 @@ class BoardFactory {
                 array[i][j] = nullptr;
             }
         }
+    }
+    //changes array in argument, does not create a new one
+    //array passed in MUST be all nullptr
+    //Fischer-random needs test cases, and this function needs retesting
+    void CreateBoard(Piece* array[][8], ChessType type = NORMAL) {
+        FreeBoard(array);
 
         Piece* pawn;
         {
