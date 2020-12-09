cf8c9551db5d7c9bfb731861d644b6f298447599
Lyran312 <ryanlu312@gmail.com>
2020-12-09 12:50:50 -0800

    Added decorator to the factory that can be used for testing

--- testing/Board_test.h
+++ testing/Board_test.h
@@ -6,37 +6,43 @@
 
 /* (BW) signifies who's turn was last
 Checkmate: https://www.chess.com/forum/view/general/13-checkmates-you-must-know
+Stalemate: https://www.ichess.net/blog/stalemate-in-chess/
     0 = normal
     1 = 2 turn win (B)
     2 = Two major pieces back rank mate (B)
     3 = Diagonal checkmate (W)
     4 = Bishop and Knight fianchetto checkmate (W)
+
+    6 = One Pawn (W)
+    7 = Two Rooks (B)
+    8 = One Queen (W)
 */
 class TestFactory : public BoardFactory {
     BoardFactory b;
 
     public:
     TestFactory() {}
-    void BulidTests (int array[][8], int input = 0) {
+    void BuildTests (int array[][8], int input = 0) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++)
                 array[i][j] = nullptr;
         }
 
         switch (input) {
-            case 0:
-                CreateBoard(array);
-                break;
             case 1:
                 CreateBoard(array);
                 array[5][5] = array[5][6];
                 array[5][6] = nullptr;
+                array[5][5]->setCoord('f',6);
                 array[4][2] = array[4][1];
                 array[4][1] = nullptr;
+                array[4][2]->setCoord('e',3);
                 array[6][4] = array[6][6];
                 array[6][6] = nullptr;
+                array[6][4]->setCoord('e',3);
                 array[7][4] = array[3][0];
                 array[3][0] = nullptr;
+                array[7][4] = setCoord('f',3);
                 break;
             case 2:
                 array[7][7] = new King(WHITE,'h',8);
@@ -82,6 +88,32 @@ class TestFactory : public BoardFactory {
                 array[7][6] = new Pawn(WHITE,'h',7);
                 array[5][7] = new Rook(WHITE,'f',8);
                 array[6][7] = new King(WHITE,'g',8);
+                break;
+
+            //-----------------------------
+
+                case 6:
+                array[3][0] = new King(BLACK,'d',1);
+                array[3][2] = new King(WHITE,'d',3);
+                array[3][1] = new Pawn(WHITE,'d',2);
+                break;
+
+                case 7:
+                array[7][7] = new King(WHITE,'h',8);
+                array[0][0] = new King(BLACK,'a',1);
+                array[0][6] = new Rook(BLACK,'a',7);
+                array[6][0] = new Rook(BLACK,'g',1);
+                break;
+
+                case 8:
+                array[0][0] = new King(BLACK,'a',1);
+                array[3][5] = new King(WHITE,'d',6);
+                array[2][1] = new Queen(WHITE,'c',2);
+
+            //-----------------------------
+
+                default:
+                CreateBoard(array);
                 break;
         }
     }
