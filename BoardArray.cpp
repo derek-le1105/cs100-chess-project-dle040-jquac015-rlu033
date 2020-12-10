5841f0acf95a0cdb02e98c070c10cbc61c739fe1
jquac015 <jquac015@ucr.edu>
2020-12-10 00:36:52 -0800

    Debugged BoardArray fusion with BoardFactory

--- BoardArray.cpp
+++ BoardArray.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
 #include "BoardArray.h"
 #include "Piece.cpp"
-#include "board.hpp"
 #include "string.h"
 using namespace std;
 
-BoardArray::BoardArray(){ 
-        boardarray = new (Piece**)[8];
-        for(int i=0; i<8; i++){
-                boardarray[i] = new (Piece*)[8];
-        }
-        for(int i=0; i<8; i++){//initialize board to be filled w/ null ptrs
-                for(int j=0; j<8; j++){
-                        boardarray[i][j] = NULL;
-                }
-        }
-}
+BoardArray::BoardArray(){}
 
-BoardArray::~BoardArray(){
-        for(int i=0; i<8; i++){
-                delete [] boardarray[i];
-        }
-        delete [] boardarray;
-}
+BoardArray::~BoardArray(){}
 
 void BoardArray::ResetBoard(){
-    CreateBoard(boardarray, NORMAL);
+    b.CreateBoard(boardarray, NORMAL);
     turn = WHITE;
 }
 
@@ -58,8 +42,8 @@ int BoardArray::ChartoInt(char toInt){
 void BoardArray::move(string col ,int row, string newcol, int newrow){
     int temp1 = ChartoInt(StringtoChar(col));
     int temp2 = ChartoInt(StringtoChar(newcol));
-    if(boardarray[temp1][row].getalignment()==turn){
-        if(boardarray[temp1][row].move(StringtoChar(newcol), newrow, boardarray)){
+    if(boardarray[temp1][row]->getAlignment()==turn){
+        if(boardarray[temp1][row]->move(StringtoChar(newcol), newrow, boardarray)){
             boardarray[temp2][newrow] = boardarray[temp1][row];
             boardarray[temp1][row] = nullptr;
             return;
@@ -92,49 +76,6 @@ bool BoardArray::check(){
 }
 
 bool BoardArray::checkmate(){
-    if(!check()){return false;}
-    Piece* checked;
-    Piece* checKing;
-    int i, j, xsave, ysave;
-    for(i=0; i<8; i++){
-        for(j=0; j<8; j++){
-            checked = boardarray[i][j];
-            if(checked->getType() == ktype && checked->getAlignment() == turn){
-                checKing = boardarray[i][j];
-            }
-        }
-    }
-    for(i=0; i<8; i++){
-        for(j=0; j<8; j++){
-            checked = boardarray[i][j];
-            if(checked->move(checKing->getX(), checKing->getY(), boardarray) && checKing->getAlignment() != checked->getAlignment()){
-                xsave = ChartoInt(checked->getX());
-                ysave = checked->getY();
-            }
-        }
-    }
-    for(i=0; i<8; i++){
-        for(j=0; j<8; j++){
-            checked = boardarray[i][j];
-            if(checked->move(xsave+'a', ysave, boardarray) && boardarray[xsave][ysave]->getAlignment() != checked->getAlignment()){
-                return false;
-            }
-        }
-    }
-    xsave = ChartoInt(checKing->getX());
-    ysave = checKing->getY();
-    for(i=ChartoInt(checKing->getX()-1); i<ChartoInt(checKing->getX())+2; i++) {
-        for(j=checKing->getY()-1; j<checKing->getY()+2; j++){
-            if(checKing->move(i+'a', j, boardarray)){
-                boardarray[i][j] = checKing;
-                checKing = nullptr;
-                check();
-            }
-        }
-    }
-    boardarray[xsave][ysave] = boardarray[i][j];
-    return false;
-}
     if(!check()){return false;}
     Piece* checked;
     Piece* checKing;
