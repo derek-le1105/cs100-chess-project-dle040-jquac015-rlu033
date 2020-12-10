05c5ddfd081f65a8c0da9d2659aa71a4b2441385
jquac015 <jquac015@ucr.edu>
2020-12-10 02:06:55 -0800

    Fixed checkmate and stalemate assigning a variable to nullptr

--- BoardArray.cpp
+++ BoardArray.cpp
@@ -87,26 +87,34 @@ bool BoardArray::checkmate(){
     int i, j, xsave, ysave;
     for(i=0; i<8; i++){
         for(j=0; j<8; j++){
-            checked = boardarray[i][j];
-            if(checked->getType() == ktype && checked->getAlignment() == turn){
-                checKing = boardarray[i][j];
+            if(boardarray[i][j] != nullptr) {
+                checked = boardarray[i][j];
+                if (checked->getType() == ktype && checked->getAlignment() == turn) {
+                    checKing = boardarray[i][j];
+                }
             }
         }
     }
     for(i=0; i<8; i++){
         for(j=0; j<8; j++){
-            checked = boardarray[i][j];
-            if(checked->move(checKing->getX(), checKing->getY(), boardarray) && checKing->getAlignment() != checked->getAlignment()){
-                xsave = ChartoInt(checked->getX());
-                ysave = checked->getY();
+            if(boardarray[i][j] != nullptr) {
+                checked = boardarray[i][j];
+                if (checked->move(checKing->getX(), checKing->getY(), boardarray) &&
+                    checKing->getAlignment() != checked->getAlignment()) {
+                    xsave = ChartoInt(checked->getX());
+                    ysave = checked->getY();
+                }
             }
         }
     }
     for(i=0; i<8; i++){
         for(j=0; j<8; j++){
-            checked = boardarray[i][j];
-            if(checked->move(xsave+'a', ysave, boardarray) && boardarray[xsave][ysave]->getAlignment() != checked->getAlignment()){
-                return false;
+            if(boardarray[i][j] != nullptr) {
+                checked = boardarray[i][j];
+                if (checked->move(xsave + 'a', ysave, boardarray) &&
+                    boardarray[xsave][ysave]->getAlignment() != checked->getAlignment()) {
+                    return false;
+                }
             }
         }
     }
@@ -138,151 +146,150 @@ bool BoardArray::stalemate() {
 
     for (i = 0; i < 8; i++) {
         for (j = 0; j < 8; j++) {
+            if(boardarray[i][j] != nullptr) {
+                currPiece = boardarray[i][j];
+                if (currPiece != nullptr) {
+                    //Pawns have 4 possible moves, but in either direction based on alignment
+                    if (currPiece->getType() == PType::ptype) {
 
-            currPiece = boardarray[i][j];
+                        for (b = currPiece->getY() - 1; b <= currPiece->getY() + 1; b = b + 2) {
+                            for (a = currPiece->getX() - 1; a <= currPiece->getY() + 1; a++) {
+                                if (currPiece->move(a, b, boardarray)) {
+                                    fromStr = {currPiece->getX()};
+                                    fromInt = currPiece->getY();
+                                    toStr = {a};
+                                    toInt = b;
 
-            if (currPiece != nullptr) {
-                //Pawns have 4 possible moves, but in either direction based on alignment
-                if (currPiece->getType() == PType::ptype) {
-
-                    for (b = currPiece->getY()-1; b <= currPiece->getY()+1; b = b+2) {
-                        for (a = currPiece->getX()-1; a <= currPiece->getY()+1; a++) {
-                            if (currPiece->move(a,b, boardarray)) {
-                                fromStr = {currPiece->getX()};
-                                fromInt = currPiece->getY();
-                                toStr = {a};
-                                toInt = b;
-
-                                if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
-                                    return false;
+                                    if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
+                                        return false;
+                                }
                             }
                         }
-                    }
 
-                    if (currPiece->move(a,b-2, boardarray) || currPiece->move(a,b+2, boardarray)) {
-                        fromStr = {currPiece->getX()};
-                        fromInt = currPiece->getY();
-                        toStr = {a};
-                        toInt = b;
-
-                        if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
-                            return false;
-                    }
-                }
-
-                    //Knights have 8 possible moves
-                else if (currPiece->getType() == PType::ntype) {
-
-                    for (b = currPiece->getY()-2; b <= currPiece->getY()+2; b = b+4) {
-                        for (a = currPiece->getX()-1; a <= currPiece->getY()+1; a = a+2) {
-                            if (currPiece->move(a,b, boardarray)) {
-                                fromStr = {currPiece->getX()};
-                                fromInt = currPiece->getY();
-                                toStr = {a};
-                                toInt = b;
-
-                                if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
-                                    return false;
-                            }
-                        }
-                    }
-                    for (b = currPiece->getY()-1; b <= currPiece->getY()+1; b = b+2) {
-                        for (a = currPiece->getX()-2; a <= currPiece->getY()+2; a = a+4) {
-                            if (currPiece->move(a,b, boardarray)) {
-                                fromStr = {currPiece->getX()};
-                                fromInt = currPiece->getY();
-                                toStr = {a};
-                                toInt = b;
-
-                                if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
-                                    return false;
-                            }
-                        }
-                    }
-                }
-
-                    //Bishops only need one space in diagonals
-                else if (currPiece->getType() == PType::btype) {
-
-                    for (b = currPiece->getY()-1; b <= currPiece->getY()+1; b = b+2) {
-                        for (a = currPiece->getX()-1; a <= currPiece->getX()+1; a = a+2) {
-                            if (currPiece->move(a,b, boardarray)) {
-                                fromStr = {currPiece->getX()};
-                                fromInt = currPiece->getY();
-                                toStr = {a};
-                                toInt = b;
-
-                                if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
-                                    return false;
-                            }
-                        }
-                    }
-                }
-
-                    //Rooks move one space vertical or horizontal
-                else if (currPiece->getType() == PType::rtype) {
-
-                    for (b = currPiece->getY()-1; b <= currPiece->getY(); b = b+2) {
-                        if (currPiece->move(currPiece->getX(),b, boardarray)) {
+                        if (currPiece->move(a, b - 2, boardarray) || currPiece->move(a, b + 2, boardarray)) {
                             fromStr = {currPiece->getX()};
                             fromInt = currPiece->getY();
-                            toStr = {currPiece->getX()};
+                            toStr = {a};
                             toInt = b;
 
                             if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
                                 return false;
                         }
                     }
-                    for (a = currPiece->getX()-1; a <= currPiece->getX(); a = a+2) {
-                        if (currPiece->move(a,currPiece->getY(), boardarray)) {
-                            fromStr = {currPiece->getX()};
-                            fromInt = currPiece->getY();
-                            toStr = {a};
-                            toInt = currPiece->getY();
 
-                            if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
-                                return false;
+                        //Knights have 8 possible moves
+                    else if (currPiece->getType() == PType::ntype) {
+
+                        for (b = currPiece->getY() - 2; b <= currPiece->getY() + 2; b = b + 4) {
+                            for (a = currPiece->getX() - 1; a <= currPiece->getY() + 1; a = a + 2) {
+                                if (currPiece->move(a, b, boardarray)) {
+                                    fromStr = {currPiece->getX()};
+                                    fromInt = currPiece->getY();
+                                    toStr = {a};
+                                    toInt = b;
+
+                                    if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
+                                        return false;
+                                }
+                            }
+                        }
+                        for (b = currPiece->getY() - 1; b <= currPiece->getY() + 1; b = b + 2) {
+                            for (a = currPiece->getX() - 2; a <= currPiece->getY() + 2; a = a + 4) {
+                                if (currPiece->move(a, b, boardarray)) {
+                                    fromStr = {currPiece->getX()};
+                                    fromInt = currPiece->getY();
+                                    toStr = {a};
+                                    toInt = b;
+
+                                    if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
+                                        return false;
+                                }
+                            }
                         }
                     }
-                }
 
-                    //Queens move as either rooks or bishops
-                else if (currPiece->getType() == PType::qtype) {
+                        //Bishops only need one space in diagonals
+                    else if (currPiece->getType() == PType::btype) {
 
-                    for (b = currPiece->getY()-1; b <= currPiece->getY()+1; b = b+1) {
-                        for (a = currPiece->getX()-1; a <= currPiece->getX()+1; a = a+1) {
-                            if (currPiece->move(a,b, boardarray)) {
+                        for (b = currPiece->getY() - 1; b <= currPiece->getY() + 1; b = b + 2) {
+                            for (a = currPiece->getX() - 1; a <= currPiece->getX() + 1; a = a + 2) {
+                                if (currPiece->move(a, b, boardarray)) {
+                                    fromStr = {currPiece->getX()};
+                                    fromInt = currPiece->getY();
+                                    toStr = {a};
+                                    toInt = b;
+
+                                    if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
+                                        return false;
+                                }
+                            }
+                        }
+                    }
+
+                        //Rooks move one space vertical or horizontal
+                    else if (currPiece->getType() == PType::rtype) {
+
+                        for (b = currPiece->getY() - 1; b <= currPiece->getY(); b = b + 2) {
+                            if (currPiece->move(currPiece->getX(), b, boardarray)) {
                                 fromStr = {currPiece->getX()};
                                 fromInt = currPiece->getY();
-                                toStr = {a};
+                                toStr = {currPiece->getX()};
                                 toInt = b;
 
                                 if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
                                     return false;
                             }
                         }
-                    }
-                }
-
-                    //actually, this is the same as the queen. Huh.
-                else if (currPiece->getType() == PType::ktype) {
-
-                    for (b = currPiece->getY()-1; b <= currPiece->getY()+1; b = b+1) {
-                        for (a = currPiece->getX()-1; a <= currPiece->getX()+1; a = a+1) {
-                            if (currPiece->move(a,b, boardarray)) {
+                        for (a = currPiece->getX() - 1; a <= currPiece->getX(); a = a + 2) {
+                            if (currPiece->move(a, currPiece->getY(), boardarray)) {
                                 fromStr = {currPiece->getX()};
                                 fromInt = currPiece->getY();
                                 toStr = {a};
-                                toInt = b;
+                                toInt = currPiece->getY();
 
                                 if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
                                     return false;
+                            }
+                        }
+                    }
+
+                        //Queens move as either rooks or bishops
+                    else if (currPiece->getType() == PType::qtype) {
+
+                        for (b = currPiece->getY() - 1; b <= currPiece->getY() + 1; b = b + 1) {
+                            for (a = currPiece->getX() - 1; a <= currPiece->getX() + 1; a = a + 1) {
+                                if (currPiece->move(a, b, boardarray)) {
+                                    fromStr = {currPiece->getX()};
+                                    fromInt = currPiece->getY();
+                                    toStr = {a};
+                                    toInt = b;
+
+                                    if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
+                                        return false;
+                                }
+                            }
+                        }
+                    }
+
+                        //actually, this is the same as the queen. Huh.
+                    else if (currPiece->getType() == PType::ktype) {
+
+                        for (b = currPiece->getY() - 1; b <= currPiece->getY() + 1; b = b + 1) {
+                            for (a = currPiece->getX() - 1; a <= currPiece->getX() + 1; a = a + 1) {
+                                if (currPiece->move(a, b, boardarray)) {
+                                    fromStr = {currPiece->getX()};
+                                    fromInt = currPiece->getY();
+                                    toStr = {a};
+                                    toInt = b;
+
+                                    if (!this->MoveBecomesCheck(fromStr, fromInt, toStr, toInt))
+                                        return false;
+                                }
                             }
                         }
                     }
                 }
             }
-
         }
     }
 
